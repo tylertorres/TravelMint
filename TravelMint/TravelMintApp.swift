a02fa7980fac5028aa9ea460d47715ecf230b49c
tylertorres <tylertorresp@gmail.com>
2023-12-27 15:03:55 -0500

    Initial Commit

---
+++ TravelMint/TravelMintApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelMintApp.swift
+//  TravelMint
+//
+//  Created by Tyler Torres on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelMintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
