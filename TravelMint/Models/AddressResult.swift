a7acb7f9a4976f432b66cbc864a5b1dc114577f1
tylertorres <tylertorresp@gmail.com>
2023-12-27 15:08:30 -0500

    Initial structure for travel mint app. Location based event search when on vacation.

---
+++ TravelMint/Models/AddressResult.swift
@@ -0,0 +1,14 @@
+//
+//  AddressResult.swift
+//  TravelMint
+//
+//  Created by Tyler Torres on 12/27/23.
+//
+
+import Foundation
+
+struct AddressResult: Identifiable {
+    let id: UUID = UUID()
+    let title: String
+    let subtitle: String
+}
---
+++ TravelMint/Screens/Search/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  TravelMint
+//
+//  Created by Tyler Torres on 12/27/23.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
